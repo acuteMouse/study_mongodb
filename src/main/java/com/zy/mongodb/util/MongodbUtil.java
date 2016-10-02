2a859ffb4ac7c597813c8d47dd2a8ab67db3cc8c
acuteMouse <1012592349@qq.com>
2016-10-02 22:25:13 +0800

    mongodb study

---
+++ src/main/java/com/zy/mongodb/util/MongodbUtil.java
@@ -0,0 +1,53 @@
+package com.zy.mongodb.util;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * @autor cgl
+ * @time 2016/9/28.
+ * 作用:
+ */
+public class MongodbUtil {
+     static MongoClient client=null;
+    /**
+     * 使用用户名、密码连接指定的database
+     * @param host
+     * @param database
+     * @param name
+     * @param password
+     * @return
+     */
+    public static  MongoDatabase getMongodbDataBase(String host,String database,String name,String password){
+        client=new MongoClient(host,27017);
+        MongoDatabase mongoDatabase =client.getDatabase(database);
+        return mongoDatabase;
+
+    }
+
+    /**
+     * 默认连接，不实用用户名和密码
+     * @param host 主机ip
+     * @param database 数据库名
+     * @return
+     */
+    public static  MongoDatabase getMongodbDataBase(String host,String database){
+        client=new MongoClient(host,27017);
+        MongoDatabase mongoDatabase =client.getDatabase(database);
+        return mongoDatabase;
+    }
+
+    /**
+     * 获取本地mongodb数据库
+     * @param database 数据库名
+     * @return
+     */
+    public static  MongoDatabase getDefaultMongodbDataBase(String database){
+        client=new MongoClient("localhost",27017);
+        MongoDatabase mongoDatabase =client.getDatabase(database);
+        return mongoDatabase;
+    }
+    public void closeConnection(){
+        client.close();
+    }
+}
---
+++ src/test/java/com/zy/mongodb/util/MongodbUtilTest.java
@@ -0,0 +1,103 @@
+package com.zy.mongodb.util;
+
+import com.mongodb.Block;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.set;
+
+/**
+ * @autor cgl
+ * @time 2016/10/2.
+ * 作用:
+ */
+public class MongodbUtilTest {
+    MongoDatabase database = null;
+    MongoCollection<Document> stud = null;
+    Block<Document> printDo = document -> System.out.println(document.toJson());
+
+    @Before
+    public void testGetDefaultMongodbDataBase() {
+        database = MongodbUtil.getDefaultMongodbDataBase("student");//数据库
+        stud = database.getCollection("stud");
+    }
+
+    @Test
+    public void testClose() {
+
+    }
+
+    /**
+     * 查询所有
+     */
+    @Test
+    public void findAll() {
+        for (Document document : stud.find()) {
+            System.out.println(document.toJson());
+        }
+    }
+
+    @Test
+    public void count() {
+        Assert.assertEquals("不相等", 103, stud.count());
+    }
+
+    /**
+     * 查询某个记录 eq  等于
+     */
+    @Test
+    public void findOne() {
+        Document document = stud.find(eq("i", 1)).first();
+        System.out.println(document.toJson());
+    }
+
+    /**
+     * gt  大于
+     */
+    @Test
+    public void gtFind() {
+        stud.find(gt("i", 60)).forEach(printDo);
+    }
+
+
+    /**
+     * gt ---lt  大于---小于
+     */
+    @Test
+    public void gtAndLt() {
+        stud.find(and(gt("i", 50), lt("i", 60))).forEach(printDo);
+    }
+
+    /**
+     * 插入一条记录，insertOne()  参数：doucument对象
+     * 插入多条  inserMany()  参数：list<Document>
+     */
+    @Test
+    public void insert() {
+        Document document = new Document();
+        document.put("name", "张三");
+        document.append("age", 12);
+        stud.insertOne(document);
+        List<Document> documents = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            documents.add(new Document("i", i));
+        }
+        stud.insertMany(documents);
+    }
+    /**
+     * 修改某个值
+     */
+    @Test
+    public void update(){
+        stud.updateOne(eq("i",1),set("i",13333));
+    }
+}
+
